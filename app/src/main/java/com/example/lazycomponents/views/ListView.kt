bf63dabae309e2797a0ebb5bedce8b640dd2bf1d
Dean Martin García <dean.martin@gracia.lasalle.cat>
2026-01-11 17:53:32 +0100

    feat(new function): Creamos la vista de MonsterItem para asi reutilizar las propiedades y que todas las tarjetas luzcan por igual.

---
+++ app/src/main/java/com/example/lazycomponents/views/ListView.kt
@@ -0,0 +1,7 @@
+package com.example.lazycomponents.views
+
+import android.widget.ListView
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ListView
---
+++ app/src/main/java/com/example/lazycomponents/views/MonsterItem.kt
@@ -0,0 +1,48 @@
+package com.example.lazycomponents.views
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import com.example.lazycomponents.model.Monster
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun MonsterItem (monster: Monster, onSelectedItem: (String) -> Unit) {
+    Card(modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onSelectedItem(monster.name) }
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = monster.image),
+                contentDescription = monster.name,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxHeight()
+            )
+            Column {
+                Text(
+                    text = monster.name,
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = monster.type
+                )
+            }
+        }
+    }
+}
